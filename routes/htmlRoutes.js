4395dae3df321cc066f8a7deffcbd882ccc0e577
JohnDaise <jdaise@gmail.com>
2020-04-15 13:49:32 -0400

    updated route

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,6 +2,10 @@ var path = require("path");
 var router = require("express").Router();
 
 // "/notes" responds with the notes.html file
+router.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
 router.get("/notes", function(req, res) {
   res.sendFile(path.join(__dirname, "../public/notes.html"));
 });
